ef7a7b2fc090c855ba0d01bb5d75f337baf504ec
Malinde Godwin <malindegodwin9@gmail.com>
2025-07-19 21:02:28 +0300

    first commit

--- frontend/src/challenge/challengeGenerator.jsx
+++ frontend/src/challenge/challengeGenerator.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import "react";
+import { useState, useEffect } from "react";
+import { MCQChallenge } from "./MCQChallenge.jsx";
 
 const ChallengeGenerator = () => {
+  const [challenge, setChallenge] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [difficulty, setDifficulty] = useState("easy");
+
+  const fetchQuota = async () => {};
+
+  const generateChallenge = async () => {};
+
+  const getNextResetTime = () => {};
+
   return <div></div>;
 };
 
