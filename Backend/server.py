82ac84ba53ca0f7c92f7c6cea329cb83f9ac8aed
Malinde Godwin <malindegodwin9@gmail.com>
2025-08-17 08:35:41 +0300

    commit

--- Backend/server.py
+++ Backend/server.py
@@ -3,9 +3,4 @@ from src.app import app
 if __name__ == "__main__":
     import uvicorn
     
-    uvicorn.run(app, host="0.0.0.0", port=8000)kdkddlcdld
-    dkjdkjhdkjfdhkfddfd
-    dfdjfdkfhdfdf
-    dfkjdfkldhfldfdflkdfdlf
-    
-    dfnkdlfhdlfndlndmdljf
+    uvicorn.run(app, host="0.0.0.0", port=8000)
